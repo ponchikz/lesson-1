d1056b33e49c7504aa8297f9cc552bb278996989
Oleg <hrapin@list.ru>
2022-04-24 15:45:55 +0800

    Homework 1

---
+++ MainApp.java
@@ -0,0 +1,67 @@
+// Первый раз 21.04.22 //
+public class MainApp {
+    public static void main(String[] args) {
+        printThreeWords();
+        checkSumSign();
+        printColor();
+        comparNumbers();
+    }
+    /*
+    Создайте метод printThreeWords(), который при вызове должен отпечатать в столбец три слова: Orange, Banana, Apple.
+     */
+    public static void printThreeWords() {
+        System.out.println("Orange");
+        System.out.println("Banana");
+        System.out.println("Apple");
+    }
+    /*
+    Создайте метод checkSumSign(), в теле которого объявите две int переменные a и b, и
+    инициализируйте их любыми значениями, которыми захотите. Далее метод должен
+    просуммировать эти переменные, и если их сумма больше или равна 0, то вывести в консоль
+    сообщение “Сумма положительная”, в противном случае - “Сумма отрицательная”
+     */
+    public static void checkSumSign() {
+        int a = 1, b = -1;
+
+        if (a + b >= 0) {
+            System.out.println("Сумма положительная");
+        }
+        else {
+            System.out.println("Сумма отрицательна");
+        }
+    }
+    /*
+    Создайте метод printColor() в теле которого задайте int переменную value и инициализируйте
+    ее любым значением. Если value меньше 0 (0 включительно), то в консоль метод должен
+    вывести сообщение “Красный”, если лежит в пределах от 0 (0 исключительно) до 100 (100
+    включительно), то “Желтый”, если больше 100 (100 исключительно) - “Зеленый”;
+     */
+    public static void printColor() {
+        int value = 101;
+
+        if (value <= 0) {
+            System.out.println("Красный");
+        }
+        if (value >= 1 && value <= 100) {
+            System.out.println("Желтый");
+        }
+        if (value > 100) {
+            System.out.println("Зелёный");
+        }
+    }
+    /*
+    Создайте метод compareNumbers(), в теле которого объявите две int переменные a и b, и
+    инициализируйте их любыми значениями, которыми захотите. Если a больше или равно b, то
+    необходимо вывести в консоль сообщение “a >= b”, в противном случае “a < b”
+    */
+    public static void comparNumbers() {
+        int a = 20, b = 55;
+
+        if (a >= b) {
+            System.out.println("a >= b");
+        }
+        else {
+            System.out.println("a < b");
+        }
+    }
+}
